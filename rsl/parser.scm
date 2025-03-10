d4d4ebf00859dfa6a88c5d700b1d9e0471c84121
Isaac Hiram Lopez Diaz <isaac.lopez@upr.edu>
2025-03-10 18:31:27 -0400

    started parser combinators

--- rsl/parser.scm
+++ rsl/parser.scm
@@ -1,7 +1,24 @@
-;; Author: Isaac H. Lopez <isaac.lopez@upr.edu>
-;; Description: Parser for language
+;; Author: Isaac H. Lopez Diaz
+;; Description: Main parser module
+(load "structs.scm")
 
-;; Main structure for AST
+;; Main parser function
 (define parser
-  (lambda (tok)
-    (
+  (lambda (f i)
+    (f i)))
+;; Given a predicate return a parser
+(define satisfy
+  (lambda (pred)
+    (lambda (stream)
+      (cond
+       ((null? stream) (list 'eof))
+       ((pred (car stream))
+	(make-either '() (make-tuple (car stream)
+				     (cdr stream))))
+       (else
+	(make-either (error 'unexpected (char->name (car stream))) '()))))))
+
+;; Given a char cheq if the stream matches it
+(define char
+  (lambda (c)
+    (satisfy (lambda (i) (char=? i c)))))
---
+++ rsl/structs.scm
@@ -0,0 +1,8 @@
+;; Author: Isaac H. Lopez Diaz <isaac.lopez@upr.edu>
+;; Description: Main structures for the parser
+
+;; tuple
+(define-structure tuple fst snd)
+
+;; either
+(define-structure either left right)
