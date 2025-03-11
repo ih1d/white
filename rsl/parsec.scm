af9279e14c4be4d9e4c11d312346e095c78ed69c
Isaac Hiram Lopez Diaz <ih1d@loki>
2025-03-11 05:55:18 -0400

    started parser combinators library

---
+++ rsl/parsec.scm
@@ -0,0 +1,46 @@
+;; Author: Isaac H. Lopez Diaz
+;; Description: Parser Combinators
+
+;; Tuple structure
+(define-structure Tuple fst snd)
+
+(define succeed
+  (lambda (v inp)
+    (cons (make-Tuple v inp)
+	  '())))
+
+(define fail
+  (lambda (inp)
+    '()))
+
+(define satisfy
+  (lambda (p)
+    (lambda (l)
+      (cond
+       ((null? l)
+	(fail '()))
+       ((p (car l))
+	(succeed (car l)
+		 (cdr l)))
+       (else
+	(fail (cdr l)))))))
+
+(define literal
+  (lambda (x)
+    (satisfy (lambda (i) (char=? i x)))))
+
+(define alt
+  (lambda (p1 p2)
+    (lambda (inp)
+      (append (p1 inp)
+	      (p2 inp)))))
+
+(define then
+  (lambda (p1 p2)
+    (lambda (inp)
+      (let ((t1 (car (p1 inp))))
+	(let ((t2 (car (p2 (Tuple-snd t1)))))
+	  (make-Tuple
+	   (make-Tuple (Tuple-fst t1)
+		       (Tuple-fst t2))
+		(Tuple-snd t2)))))))
