d52afc98bd0dcfb58d89457303532d13ba08fb69
Isaac Hiram Lopez Diaz <ih1d@loki>
2025-03-11 03:47:10 -0400

    continue parser combinator

--- rsl/parser.scm
+++ rsl/parser.scm
@@ -1,26 +1,42 @@
 ;; Author: Isaac H. Lopez Diaz
 ;; Description: Main parser module
 
+;; Parser main structure
+(define-structure Parser input either)
+
+;; Either struct
+(define-structure Either left right)
+
 ;; Main parser function
 (define parser
   (lambda (f i)
-    (f i)))
+    (let ((p (make-Parser i (make-Either '() '()))))
+      (f p))))
 
 ;; given a predicate return true if it worked and modify the stream
 ;; else return false with an error
 (define satisfy
   (lambda (pred)
-    (lambda (stream)
+    (lambda (p)
       (cond
-       ((null? stream)
-	(list 'eof))
-       ((pred (car stream))
-	(list (car stream)
-	      (cdr stream)))
+       ((null? (Parser-input p))
+	(set-Parser-either! p (make-Either (list 'eof) '()))
+	(Either-left (Parser-either p)))
+       ((pred (car (Parser-input p)))       
+	(set-Parser-either! p (make-Either '() (list (car (Parser-input p)) (cdr (Parser-input p)))))
+	(Either-right (Parser-either p)))
        (else
-	(error (car stream) "unexpected"))))))
+	(set-Parser-either! p (make-Either (list 'unexpected
+					       (car (Parser-input p)))
+					 '()))
+	(Either-left (Parser-either p)))))))
 
 ;; Given a char match if the current stream satisfies the condition
 (define char
   (lambda (c)
     (satisfy (lambda (i) (char=? i c)))))
+
+;; Check if the next character is a digit
+(define digit
+  (lambda ()
+    (satisfy (lambda (i) (char-numeric? i)))))
