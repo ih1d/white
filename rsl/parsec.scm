e13ecc316f1e635fab0d4a3670d644c93a3e6b88
Isaac Hiram Lopez Diaz <isaac.lopez@upr.edu>
2025-03-11 11:31:34 -0400

    fixed then combinator

--- rsl/parsec.scm
+++ rsl/parsec.scm
@@ -41,7 +41,8 @@
     (lambda (inp)
       (let ((t1 (car (p1 inp))))
 	(let ((t2 (car (p2 (Tuple-snd t1)))))
-	  (make-Tuple
-	   (make-Tuple (Tuple-fst t1)
-		       (Tuple-fst t2))
-	   (Tuple-snd t2)))))))
+	  (list
+	   (make-tuple
+	    (make-Tuple (Tuple-fst t1)
+		        (Tuple-fst t2))
+	   (Tuple-snd t2))))))))
