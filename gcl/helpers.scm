4edbe07a955e440a7a8db78b02d93cf8ce77cf27
Isaac Hiram Lopez Diaz <isaac.lopez@upr.edu>
2025-03-25 15:46:15 -0400

    added main file

--- gcl/helpers.scm
+++ gcl/helpers.scm
@@ -7,3 +7,24 @@
   (lambda (p q)
     (or (not p)
 	q)))
+
+;; take from list while predicate is true
+(define take-while
+  (lambda (pred lat)
+    (cond
+     ((null? lat) '())
+     ((pred (car lat))
+      (cons (car lat)
+	    (take-while pred (cdr lat))))
+     (else '()))))
+
+;; drop from list while predicate is true
+(define drop-while
+  (lambda (pred lat)
+    (cond
+     ((null? lat) '())
+     ((pred (car lat))
+      (drop-while pred (cdr lat)))
+     (else lat))))
+
+      
--- gcl/scanner.scm
+++ gcl/scanner.scm
@@ -1,3 +1,31 @@
 ;; Author: Isaac H. Lopez Diaz <isaac.lopez@upr.edu>
 ;; Description: Scanner for GCL
-;; Licensed under GPLv3
+;; Licensed under GPLv2
+(load "helpers.scm")
+
+;; entry structure
+(define-structure entry type lexeme)
+
+;; Main scanner function
+(define scanner
+  (lambda (l)
+    (cond
+     ((null? l) '())
+     ((number? (car l))
+      (let ((full-number (take-while number? l)))
+	(cons (make-entry 'number full-number)
+	      (scanner (drop-while number? l)))))
+     ((eq? '+ (car l))
+      (cons (make-entry 'plus (car l))
+	    (scanner (cdr l))))
+     ((eq? '- (car l))
+      (cons (make-entry 'minus (car l))
+	    (scanner (cdr l))))
+     ((eq? '* (car l))
+      (cons (make-entry 'mult (car l))
+	    (scanner (cdr l))))
+     ((eq? '/ (car l))
+      (cons (make-entry 'div (car l))
+	    (scanner (cdr l))))
+     (else
+      (error (car l) "not defined")))))
