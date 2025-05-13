935a111c41db05bd26bce8855ef86014a4e779e3
Isaac Hiram Lopez Diaz <isaac.lopez@upr.edu>
2025-05-13 16:23:14 -0400

    finished reflective interpreter

--- lambda.scm
+++ lambda.scm
@@ -1,44 +1,70 @@
 ;; Author: Isaac H. Lopez <isaac.lopez@upr.edu>
 ;; Description: Reflective lambda calculus interpreter
 
-;; Main eval function
-(define (l-eval expr env)
-  (if (list? expr)
-      (cond
-       ((single? expr)
-	(l-eval (car expr) env cont))
-       ((eq? 'lambda (car expr))
-	(lambda (arg)
-	  (l-eval (third expr) (lambda (y)
-			       (if (eq? y (second expr))
-				   arg
-				   (env y)))
-		  cont)))
-       ((pair? expr)
-	((l-eval (car expr) env cont) (l-eval (cdr expr) env cont)))
-       (else (error (car expr) "not a valid expression -- EVAL")))
-      (cond
-       ((number? expr) expr)
-       ((symbol? expr) (env expr))
-       (else (error expr "not a valid expression -- EVAL")))))
+;; expression predicates
+(define (var? e)
+  (symbol? e))
 
-(define (single? ls)
-  (= (length ls) 1))
+(define (lambda? e)
+  (eq? (car e) 'lambda))
+
+(define (app? e)
+  (pair? e))
+
+;; expression accessors
+(define (lambda-id e)
+  (cadr e))
+
+(define (lambda-body e)
+  (caddr e))
+
+(define (app-func e)
+  (car e))
+
+(define (app-arg e)
+  (cadr e))
+
+;; Main eval function
+(define (l-eval expr env cont)
+  (cond ((var? expr) (env expr cont))
+	((lambda? expr)
+	 (lambda (arg)
+	   (l-eval (lambda-body expr) (lambda (y)
+					(if (eq? (lambda-id expr))
+					    arg
+					    (env y)))
+		   cont)))
+	((app? expr)
+	 ((l-eval (app-func expr) env cont) (l-eval (app-arg expr) env cont)))
+	(else
+	 (write expr) (display " is not defined -- EVAL")
+	 (car (force cont)))))
 
 ;; Environment functions
 (define the-empty-environment
-  (lambda (y)
-    (error y "variable not defined")))
+  (lambda (y cont)
+    (write y) (display " variable not defined") (newline)
+    (let* ((base-cont (force cont))
+	   (call (car base-cont))
+	   (mcont (cdr base-cont)))
+      (call mcont))))
 
-(define init-env
-  (lambda (y)
-    (cond
-     ((eq? y 'zero) 0)
-     (else the-empty-environment))))
+;; setup
+(define (start-interp level turn env cont)
+  (write level) (write '-) (write turn) (display "> ")
+  (let* ((r (read))
+	 (ans (l-eval r env cont)))
+    (newline) (display ans) (newline)
+    (start-interp level (+ turn 1) env cont)))
 
-(define (start)
-  (display "> ")
-  (let ((r (read))
-	(ans (l-eval r init-env)))
-    (newline) (display ans)
-    (start)))
+(define (init-cont level turn env)
+  (display "New level loaded.") (newline)
+  (cons-stream (lambda (cont)
+		 (start-interp level turn env cont))
+	       (init-cont (+ level 1) turn env)))
+
+(define (white)
+  (let* ((base (init-cont 0 0 the-empty-environment))
+	 (cont (car base))
+	 (mcont (cdr base)))
+    (cont mcont)))
