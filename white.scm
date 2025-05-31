b98d286d4d2feda80a2e3aeb9375562b4e8c58d6
Isaac Lopez <isaac.lopez@upr.edu>
2025-05-31 07:44:17 -0400

    started meta circular evaluator

--- white.scm
+++ white.scm
@@ -1,4 +1,93 @@
 ;; Author: Isaac H. Lopez <isaac.lopez@upr.edu>
-;; Description: Denotational Semantics Metalanguage
+;; Description: Reflective language
 
 ;; expression
+(define (var? e)
+  (symbol? e))
+
+(define (constant? e)
+  (or (number? e)
+      (boolean? e)))
+
+(define (if? e)
+  (eq? (car e) 'if))
+
+(define (let? e)
+  (eq? (car e) 'let))
+
+(define (lambda? e)
+  (eq? (car e) 'lambda))
+
+(define (app? e)
+  (pair? e))
+
+;; accessors
+(define (if-pred e)
+  (cadr e))
+
+(define (if-then e)
+  (caddr e))
+
+(define (if-else e)
+  (cadddr e))
+
+(define (let-var&vals e)
+  (define (loop pairs)
+    (cond ((null? pairs) '())
+	  (else (cons (car pairs)
+		      (loop (cdr pairs))))))
+  (loop (cadr e)))
+
+(define (let-body e)
+  (caddr e))
+
+(define (lambda-id e)
+  (car (cadr e)))
+
+(define (lambda-body e)
+  (caddr e))
+
+;; eval
+(define (meta-eval expr env)
+  (cond ((constant? expr) expr)
+	((var? expr) (lookup expr env))
+	((let? expr) (eval-let expr env))
+	((if? expr) (eval-if expr env))       
+	((lambda? expr) (eval-lambda expr env))
+	((app? expr) (meta-apply expr env))))
+
+(define (eval-if expr env)
+  (if (meta-eval (if-pred expr) env)
+      (meta-eval (if-then expr) env)
+      (meta-eval (if-else expr) env)))
+
+(define (eval-let expr env)
+  (let ((new-env (map (lambda (pair)
+			(extend-environment (first pair) (second pair) env))
+		      (let-var&vals expr))))
+    (meta-eval (let-body expr) new-env)))
+
+(define (eval-lambda expr env)
+  (lambda (arg) (meta-eval (lambda-body expr) (extend-environment (lambda-id expr) arg env))))
+
+;; environment
+(define the-empty-environment '())
+
+(define (lookup var env)
+  (cond ((null? env) (error var "unbound variable -- LOOKUP"))
+	((eq? var (car (car env)))
+	 (cdr (car env)))
+	(else (lookup var (cdr env)))))
+
+(define (extend-environment var val env)
+  (if (null? env)
+      (cons var val)
+      (cons (cons var val) env)))
+
+;; main
+(define (white)
+  (display "> ")
+  (let ((r (read)))
+    (display (meta-eval r the-empty-environment)) (newline)
+    (white)))
+	
